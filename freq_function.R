da1d30609938246dc62e6efc49322ba5a2900af3
Kazuho Kambara <>
2020-03-02 19:08:49 +0900

    Update freq_function

--- freq_function.R
+++ freq_function.R
@@ -1,12 +1,25 @@
 # 石川 (2012: Ch.6) の共起頻度の評価指標の為のスクリプト
 freq_test <- function(collocation, x, y, n){
+  ## 観察値と期待値の算出
+  observed <- matrix(c(
+    collocation, x-collocation, y-collocation, 
+    (n-(y-collocation)-collocation-(x-collocation))), byrow=T, ncol=2)
+  observed_with_sum <- addmargins(observed)
+  expected <- matrix(c(
+    (observed_with_sum[1,3]*observed_with_sum[3,1]/observed_with_sum[3,3]), 
+    (observed_with_sum[1,3]*observed_with_sum[3,2]/observed_with_sum[3,3]), 
+    (observed_with_sum[2,3]*observed_with_sum[3,1]/observed_with_sum[3,3]), 
+    (observed_with_sum[2,3]*observed_with_sum[3,2]/observed_with_sum[3,3])), ncol=2, byrow=T)
+  ## 各指標の計算
   Dice <- 2*(collocation / (x + y));
   Jaccard <- collocation / (x + y - collocation);
   Cosine <- collocation / ({sqrt(x)}*{sqrt(y)});
   Simpson <- collocation / (min(x, y));
   MI <- log({(collocation*n)/(x*y)}, 2);
-  result <- c(Dice, Jaccard, Cosine, Simpson, MI)
-  names(result) <- c("Dice", "Jaccard", "Cosine", "Simpson", "MI")
+  sqrtG <- 2*(sum(collocation*(sum(log(observed) - log(expected)))));
+  t <- (1/sqrt(collocation))*(collocation - (x*y/n));
+  result <- c(Dice, Jaccard, Cosine, Simpson, MI, sqrtG, t);
+  names(result) <- c("Dice", "Jaccard", "Cosine", "Simpson", "MI", "sqrtG", "t");
   return(result)
 }
 
@@ -18,7 +31,7 @@ beautiful_countryside <- 48
 beautiful_day <- 46
 beautiful_house <- 54
 beautiful_hair <- 35
-F <- c(beautiful_woman, beautiful_place, beautiful_countryside, beautiful_day, beautiful_house, beautiful_hair); F
+Freq <- c(beautiful_woman, beautiful_place, beautiful_countryside, beautiful_day, beautiful_house, beautiful_hair); Freq
 
 # コーパス全体の語数（BNCにおける語数は一億
 N <- 96263399 
@@ -37,19 +50,21 @@ Y <- c(woman, place, countryside, day, house, hair); Y
 
 # 保存用のベクトルの生成 (cf. Gries 2013: 96)
 collocations <- c("beautiful woman", "beautiful place", "beautiful countryside", "beautiful day", "beautiful house", "beautiful hair")
-Dice <- vector(length=length(F))
-Jaccard <- vector(length=length(F))
-Cosine <- vector(length=length(F))
-Simpson <- vector(length=length(F))
-MI <- vector(length=length(F))
+Dice <- vector(length=length(Freq))
+Jaccard <- vector(length=length(Freq))
+Cosine <- vector(length=length(Freq))
+Simpson <- vector(length=length(Freq))
+MI <- vector(length=length(Freq))
+sqrtG <- vector(length=length(Freq))
+t <- vector(length=length(Freq))
 
 #result <- cbind(result, freq_test(F[2], X, Y[2], N))
-result <- data.frame(collocations, Dice, Jaccard, Cosine, Simpson, MI); result
+result <- data.frame(collocations, Dice, Jaccard, Cosine, Simpson, MI, sqrtG, t); result
 
-for(i in 1:length(F)){
-  result[i,2:6] <- freq_test(F[i], X, Y[i], N)
+for(i in 1:length(Freq)){
+  #result[i,2:6] <- freq_test(Freq[i], X, Y[i], N)
   ## 小数点3桁で表示したい場合はこちらを使う
-  #result[i,2:6] <- round(freq_test(F[i], X, Y[i], N), digits = 3)
+  result[i,2:8] <- round(freq_test(Freq[i], X, Y[i], N), digits = 3)
 }
 
 result
